1c7a46846aa7f650ee365795f1a0350ee1387d5d
Ali <alihassnain330@gmail.com>
2024-05-03 22:51:37 +0500

    fix the login and logout issue

--- Client/src/App.js
+++ Client/src/App.js
@@ -8,21 +8,26 @@ import Category from "./components/Category/Category";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
 import AppContext from "./utils/context";
+import LoginComponent from "./components/Auth/login/Login";
+import RegisterComponent from "./components/Auth/Signup/Signup";
+
+//getting errror: You cannot render a <Router> inside another <Router>. You should never have more than one in your app.
+//Solution: Remove the BrowserRouter from the App.js file and wrap the App component with the BrowserRouter in the index.js file.
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppContext>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category/:id" element={<Category />} />
-          <Route path="/product/:id" element={<SingleProduct />} />
-        </Routes>
-        <Newsletter />
-        <Footer />
-      </AppContext>
-    </BrowserRouter>
+    <AppContext>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:id" element={<Category />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+        <Route path="/login" element={<LoginComponent />} />
+        <Route path="/signup" element={<RegisterComponent />} />
+      </Routes>
+      <Newsletter />
+      <Footer />
+    </AppContext>
   );
 }
 
--- Client/src/index.js
+++ Client/src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
+import AuthProvider from "./components/Auth/Authprovider/AuthProvider";
+import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+
+//getting errror: You cannot render a <Router> inside another <Router>. You should never have more than one in your app.
+//Solution: Remove the BrowserRouter from the App.js file and wrap the App component with the BrowserRouter in the index.js file.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
